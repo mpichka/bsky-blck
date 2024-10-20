28c0be053ff0d50847fae0c92b94c09937c89f01
Mykhailo Pichka <dusttailtale@gmail.com>
2024-10-20 11:04:04 +0300

    Added Sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./global.css";
+import "./global.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
